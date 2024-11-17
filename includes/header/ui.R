27f51f11b8e76b51d674094db94181ccd637ec09
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-11-17 14:59:37 -0500

    Create ui.R

---
+++ includes/header/ui.R
@@ -0,0 +1,6 @@
+header.main <- titlePanel(tags$div(
+  tags$img(src="logo.jpg")," ",
+  "Exploratory Data Analysis for Drug AFR",
+  tags$div(class = "hidden-mobile hidden-tablet pull-right",
+             actionButton("aboutproject", "", icon = icon("question")))
+), windowTitle = "Exploratory Data Analysis")
