9d3129eb8b5d96c1384ac10585d30732d3ea0407
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-11-17 19:28:09 -0500

    Create ui.R

---
+++ includes/body/data/ui.R
@@ -0,0 +1,28 @@
+############################################################################
+############################################################################
+##  Document Path: includes/body/ui.R
+##
+##  Description: User interface for main body section
+##
+##  R version 4.4.1 (2024-06-14 ucrt)
+##
+#############################################################################
+#############################################################################
+
+
+# regimen setup panel
+body.panel.right.table.rawdata <- card.pro(
+
+  title = "Raw data",
+  icon = icon("book"),
+  removebtn = FALSE,
+  colorbtn = FALSE,
+  expandbtn = FALSE,
+  editbtn = TRUE,
+  collapsed = FALSE,
+  DTOutput("rhstable1"),
+  # rhandsontable(regimenDT, width = "100%") %>%
+  #   hot_context_menu(allowRowEdit = FALSE, allowColEdit = FALSE),
+
+  footer = "Legend: Group - treatment group, Dose - dose in mg, Frequency - dosing frequency as integer, Additional - number of additional doses, Route - route of administration, F1 - bioavilability for the group"
+)
