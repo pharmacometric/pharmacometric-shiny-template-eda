2347367b7acb8048220f856f884a50fc41f6190f
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-11-21 17:46:27 -0500

    Update server.R

--- includes/body/data/server.R
+++ includes/body/data/server.R
@@ -3,22 +3,34 @@ originalData <- reactive({
 })
 
 dataV2 <- reactive({
-  eval(parse(text = paste0("subset(originalData(),",input$subsetting1,")")))
+  tryCatch(eval(parse(text = paste0("subset(originalData(),",input$subsetting1,")"))),
+           error = function(e) originalData()[1,])
 })
 
 dataV3 <- reactive({
-  eval(parse(text = paste0("subset(originalData(),",input$subsetting2,")")))
+  tryCatch(eval(parse(text = paste0("subset(originalData(),",input$subsetting2,")"))),
+           error = function(e) originalData()[1,])
 })
 
 
 observeEvent(input$rundatabutton,{
-  head(originalData())
-  head(dataV3())
+  GLOBAL$data.versions <- list(
+    "original" = originalData(),
+    "dataV2" = dataV2(),
+    "dataV3" = dataV3()
+    )
 })
 
 output$rhstable1 <- renderDT(
   switch (input$datatoUseV1,
-          "original"= originalData(),"dataV2"=dataV2(),"dataV3"=dataV3()
+          "original"= GLOBAL$data.versions$original,
+          "dataV2"=GLOBAL$data.versions$dataV2,
+          "dataV3"=GLOBAL$data.versions$dataV3
   ),
   options = list(lengthChange = FALSE), filter = list(position = "top")
 )
+
+
+
+
+
