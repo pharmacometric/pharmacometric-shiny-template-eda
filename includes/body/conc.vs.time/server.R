44d30ca7a582f3594f758ff0f8e14ef9a326a0ea
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-11-24 19:47:43 -0500

    Update server.R

--- includes/body/conc.vs.time/server.R
+++ includes/body/conc.vs.time/server.R
@@ -10,6 +10,22 @@
 #############################################################################
 
 output$concvtimeplot1 <- renderPlot({
+  plot.data <- GLOBAL$data.versions[[input$datatoUseconc1]]
+  if(nrow(plot.data)){
+
+    plot.data$.dv = as.numeric(plot.data[[input$depvar1]])
+    plot.data$.tm = as.numeric(plot.data[[input$depvar2]])
+    plot.data$.colv = as.factor(plot.data[[input$colvar3]])
+    plot.data$.ttr = as.factor(plot.data[[input$depvar3]])
+
+    ggplot(data = plot.data %>% filter(not.na(.dv)),aes(.tm,.dv,color = .colv)) +
+      geom_point() +
+      geom_line() +
+      theme_bw() +
+      facet_wrap(.~.ttr)+
+      labs(x = input$labelx, y = input$labely)+
+      styler0
+  }else{
   plot(1:100,
        1:100,
        xlab = "sample x",
@@ -17,4 +33,8 @@ output$concvtimeplot1 <- renderPlot({
        ylab = "sample y"
   )
   text(50, 50, "Click 'Start simulation' to run simulations and display results", cex = 1.2, pos = 3, col = "red")
+}
 })
+
+
+
