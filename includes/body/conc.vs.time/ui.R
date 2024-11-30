5ae1618bbcff555e9a48937c4fe201ded5ad8c82
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-11-30 01:42:16 -0500

    Update ui.R

--- includes/body/conc.vs.time/ui.R
+++ includes/body/conc.vs.time/ui.R
@@ -16,26 +16,36 @@ body.panel.right.plot.conc <- card.pro(
   collapsed = FALSE,
   header.bg = "blueLight",
   xtra.header.content = textOutput("reportgraphstatus"),
-  div(id="reportgraphstatus2",
-      tags$blockquote(style="color:blue","Tabs: DV.vs.TSFD (concentration versus time since first dose) and DV.vs.TSLD (concentration versus time since last dose)")
-      ),
+  div(
+    id = "reportgraphstatus2",
+    tags$blockquote(style = "color:blue", "Tabs: DV.vs.TSFD (concentration versus time since first dose) and DV.vs.TSLD (concentration versus time since last dose)")
+  ),
   tabs = list(
     tabEntry(
       "DV.vs.TSFD",
       selectInput("datatoUseconc1", "Data version to use:", choices = data.versions.names),
       plotOutput("concvtimeplot1", height = 500)
     ),
-    tabEntry("DV.vs.TSLD",
-             selectInput("datatoUseconc2", "Data version to use:", choices = data.versions.names),
-             plotOutput("concvtimeplot2", height = 500))
+    tabEntry(
+      "DV.vs.TSLD",
+      selectInput("datatoUseconc2", "Data version to use:", choices = data.versions.names),
+      plotOutput("concvtimeplot2", height = 500)
+    )
   ),
   sidebar = div(
     tags$label("Graph settings"),
     selectInput("graphtype", "Graph type", choices = c(
-      "Combined", "Combined_group", "Facet by ID", "Facet by Group", "Facet by Dose"
+      "overall - spaghetti plot" = 1,
+      "overall - scatter plot" = 2,
+      "overall - summarised" = 3,
+      "facet - spaghetti plot" = 4,
+      "facet - scatter plot" = 5,
+      "facet - summarised" = 6,
+      "individual - spaghetti plot" = 7,
+      "individual - scatter plot" = 8
     ), selected = "Facet by Group", width = "90%"),
     conditionalPanel(
-      condition = "input.graphtype == 'Combined' | input.graphtype == 'Combined_group' | input.graphtype == 'Facet by Group'",
+      condition = "input.graphtype == 3 | input.graphtype == 6",
       selectInput("graphtype2", "Statistic", choices = c(
         "Mean", "Mean ± SD", "Mean ± SEM", "Median", "Median ± 90% PI", "Median ± 95% PI"
       ), selected = "Median ± 90% PI", width = "90%")
@@ -67,12 +77,12 @@ body.panel.right.plot.conc <- card.pro(
       max = 50,
       value = 12
     ),
-    selectInput('legendposition','Legend position',choices = c('bottom','top','left','right','none'), width = "90%"),
-    numericInput('ncollegend','Number of legend columns', value = 5, width = "90%"),
+    selectInput("legendposition", "Legend position", choices = c("bottom", "top", "left", "right", "none"), width = "90%"),
+    numericInput("ncollegend", "Number of legend columns", value = 5, width = "90%"),
     "For downloads:",
     numericInput("downimgdpi", "Image dpi", 300, width = "90%"),
     numericInput("downimgw", "Image width (px)", 2500, width = "90%"),
-    numericInput("downimgh", "Image height (px)", 2000, width = "90%"),
+    numericInput("downimgh", "Image height (px)", 2080, width = "90%"),
     numericInput("downimgs", "Image scale", 1, width = "90%"),
     br(),
     downloadButton("concvtimedownloadimg", "Download plot", icon = icon("image"))
