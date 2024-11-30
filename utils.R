b7669b45092626696b3ef0e8771c730162cd5dbb
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-11-30 03:13:38 -0500

    Update utils.R

--- utils.R
+++ utils.R
@@ -1,26 +1,35 @@
-setpath <- function(){
+############################################################################
+############################################################################
+##  Document Path: utils.R
+##
+##  Description: global functions and variables used by the app
+##
+##  R version 4.4.1 (2024-06-14 ucrt)
+##
+#############################################################################
+#############################################################################
 
-}
+library(ggplot2)
+library(grid)
+library(ggthemes)
 
-
-source.part = function(path, which = c("ui","server"), input = NULL, output = NULL,session = NULL){
-
-  which = match.arg(which)
-  for(h in list.files(path = path, pattern = paste0(which,".R$"), full.names = TRUE, recursive = TRUE)){
-    this.path = dirname(h)
+source.part <- function(path, which = c("ui", "server"), input = NULL, output = NULL, session = NULL) {
+  which <- match.arg(which)
+  for (h in list.files(path = path, pattern = paste0(which, ".R$"), full.names = TRUE, recursive = TRUE)) {
+    this.path <- dirname(h)
     source(h, local = TRUE)
   }
-
 }
 
-createSampleData <- function(N = 100){
+createSampleData <- function(N = 10) {
   ##  ID: Unique identifier for each patient (integer).
   ##  AGE: Age of the patient (float).
   ##  WEIGHT: Body weight of the patient (float).
   ##  GFR: Glomerular filtration rate (float).
   ##  RENAL_FUNCTION: Categorical variable indicating the renal function status (string).
   ##  GFR_VALUE: Numerical representation of GFR based on typical physiological values (float).
-  ##  TIME: Time post-dose (float, in hours).
+  ##  TIME: Time post first dose (float, in hours).
+  ##  TSLD: Time post last dose (float, in hours).
   ##  DOSE: Administered dose (float, in mg).
   ##  DV: Plasma concentration of the drug (float, in mg/L; set to . for dosing records).
   ##  EVID: Evidence of observation (1 for observed, 0 for not observed).
@@ -52,13 +61,13 @@ createSampleData <- function(N = 100){
 
   for (i in 1:num_patients) {
     if (age[i] < 60) {
-      gfr[i] <- round(runif(1, 60, 100), 1)  # Normal
+      gfr[i] <- round(runif(1, 60, 100), 1) # Normal
       renal_function[i] <- "Normal"
     } else if (age[i] < 75) {
-      gfr[i] <- round(runif(1, 30, 60), 1)   # Mild
+      gfr[i] <- round(runif(1, 30, 60), 1) # Mild
       renal_function[i] <- "Mild"
     } else {
-      gfr[i] <- round(runif(1, 0, 30), 1)    # Severe
+      gfr[i] <- round(runif(1, 0, 30), 1) # Severe
       renal_function[i] <- "Severe"
     }
   }
@@ -85,13 +94,13 @@ createSampleData <- function(N = 100){
       regimenDT <- rbind(regimenDT, data.frame(
         ID = i,
         AGE = age[i],
-        WT = wt[i],        # Renamed to WT
+        WT = wt[i], # Renamed to WT
         GFR = gfr[i],
         RENAL_FUNCTION = renal_function[i],
         GFR_VALUE = gfr[i],
-        TIME = j * 24,     # Assuming dosing every 24 hours
+        TIME = j * 24, # Assuming dosing every 24 hours
         TSLD = 0,
-        DOSE = dose_value,  # Use the determined dose value
+        DOSE = dose_value, # Use the determined dose value
         DV = ".",
         EVID = 1,
         DVID = 0,
@@ -99,37 +108,36 @@ createSampleData <- function(N = 100){
         SS = 0,
         FLAG = 1,
         TRT = treatment[i],
-        CMT = 1,           # Compartment for dosing
-        AMT = as.character(dose_value)  # Amount given as character
+        CMT = 1, # Compartment for dosing
+        AMT = as.character(dose_value) # Amount given as character
       ))
     }
 
     # Observation records (3 records with actual DV values)
-    omega = runif(1,0.001,0.99)
-    conctime = subset(get(paste0("r",dose_value)), TIME %in% c(0, 4, 12, 24, 36, 48, 60, 72, 84, 96))
-    #print(dose_value)
+    omega <- runif(1, 0.001, 0.99)
+    conctime <- subset(get(paste0("r", dose_value)), TIME %in% c(0, 4, 12, 24, 36, 48, 60, 72, 84, 96))
+    # print(dose_value)
     for (j in conctime$TIME) {
-
-      dv_value <- conctime[conctime$TIME==j,]$DV * exp(omega)
+      dv_value <- conctime[conctime$TIME == j, ]$DV * exp(omega)
       regimenDT <- rbind(regimenDT, data.frame(
         ID = i,
         AGE = age[i],
-        WT = wt[i],        # Renamed to WT
+        WT = wt[i], # Renamed to WT
         GFR = gfr[i],
         RENAL_FUNCTION = renal_function[i],
         GFR_VALUE = gfr[i],
         TIME = j, # * 24 + 12,  # Assuming mid-point after dosing
-        TSLD = conctime[conctime$TIME==j,]$TSLD,
+        TSLD = conctime[conctime$TIME == j, ]$TSLD,
         DOSE = dose_value,
         DV = dv_value,
-        EVID = 0,           # Changed to 0 for observations
+        EVID = 0, # Changed to 0 for observations
         DVID = 0,
         FLAG = 1,
         II = 0,
         SS = 0,
         TRT = treatment[i],
-        CMT = 2,           # Compartment for observations
-        AMT = "."          # Set to "." for observations
+        CMT = 2, # Compartment for observations
+        AMT = "." # Set to "." for observations
       ))
     }
   }
@@ -137,31 +145,29 @@ createSampleData <- function(N = 100){
   regimenDT %>% arrange(ID, TIME)
 }
 
-updateSimStatus <- function(message = "No data updates have been made."){
-  shinyjs::runjs(paste0("$('#tracksimulations').html('",message,"')"))
+updateSimStatus <- function(message = "No data updates have been made.") {
+  shinyjs::runjs(paste0("$('#tracksimulations').html('", message, "')"))
 }
 
 
-updateGraphStatus <- function(message = ""){
-  shinyjs::runjs(paste0("$('#reportgraphstatus').html('",message,"')"))
+updateGraphStatus <- function(message = "") {
+  shinyjs::runjs(paste0("$('#reportgraphstatus').html('", message, "')"))
 }
 
-updateGraphStatus2 <- function(message = ""){
-  shinyjs::runjs(paste0("$('#reportgraphstatus2').html('",message,"')"))
+updateGraphStatus2 <- function(message = "") {
+  shinyjs::runjs(paste0("$('#reportgraphstatus2').html('", message, "')"))
 }
 
-updateVariableHolder <- function(message = ""){
-  shinyjs::runjs(paste0("$('#varnamesholder').html('",message,"')"))
+updateVariableHolder <- function(message = "") {
+  shinyjs::runjs(paste0("$('#varnamesholder').html('", message, "')"))
 }
 
-disableSims <- function(is = "true"){
-  shinyjs::runjs(paste0('$("#runsimbutton").prop("disabled",',is,')'))
+disableSims <- function(is = "true") {
+  shinyjs::runjs(paste0('$("#runsimbutton").prop("disabled",', is, ")"))
 }
 
 
-library(ggplot2)
-library(grid)
-library(ggthemes)
+
 styler06 <- list(theme(
   axis.title.y = element_text(face = "bold"),
   panel.background = element_rect(colour = "#333333"),
@@ -175,30 +181,33 @@ styler00 <- list(theme(
   strip.text = element_text(face = "bold")
 ))
 
-styler03 <- theme(plot.title = element_text(face = "bold",
-            hjust = 0.5, margin = margin(0,0,20,0)),
-            text = element_text(),
-            panel.background = element_rect(colour = NA),
-            plot.background = element_rect(colour = NA),
-            panel.border = element_rect(colour = NA),
-            axis.title = element_text(face = "bold",size = rel(1)),
-            axis.title.y = element_text(angle=90,vjust =2),
-            axis.title.x = element_text(vjust = -0.2),
-            axis.text = element_text(),
-            axis.line.x = element_line(colour="black"),
-            axis.line.y = element_line(colour="black"),
-            axis.ticks = element_line(),
-            legend.key = element_rect(colour = NA),
-            legend.position = "bottom",
-            legend.direction = "horizontal",
-            legend.box = "vetical",
-            legend.key.size= unit(0.5, "cm"),
-            #legend.margin = unit(0, "cm"),
-            legend.title = element_text(face="italic"),
-            plot.margin=unit(c(10,5,5,5),"mm"),
-            strip.background=element_rect(colour="black",fill="#f0f0f0", linewidth =rel(2)),
-            strip.text = element_text(face="bold")
-    )
+styler03 <- theme(
+  plot.title = element_text(
+    face = "bold",
+    hjust = 0.5, margin = margin(0, 0, 20, 0)
+  ),
+  text = element_text(),
+  panel.background = element_rect(colour = NA),
+  plot.background = element_rect(colour = NA),
+  panel.border = element_rect(colour = NA),
+  axis.title = element_text(face = "bold", size = rel(1)),
+  axis.title.y = element_text(angle = 90, vjust = 2),
+  axis.title.x = element_text(vjust = -0.2),
+  axis.text = element_text(),
+  axis.line.x = element_line(colour = "black"),
+  axis.line.y = element_line(colour = "black"),
+  axis.ticks = element_line(),
+  legend.key = element_rect(colour = NA),
+  legend.position = "bottom",
+  legend.direction = "horizontal",
+  legend.box = "vetical",
+  legend.key.size = unit(0.5, "cm"),
+  # legend.margin = unit(0, "cm"),
+  legend.title = element_text(face = "italic"),
+  plot.margin = unit(c(10, 5, 5, 5), "mm"),
+  strip.background = element_rect(colour = "#000000", fill = "#f3f3f3", linewidth = rel(1.6)),
+  strip.text = element_text(face = "bold")
+)
 styler01 <- list(
   theme(
     axis.title.x = element_text(size = 14),
@@ -215,13 +224,18 @@ styler01 <- list(
 
 
 
-getTimeV <- function(n,t0){
-  if(n > 1) c(0, pop_off(cumsum(t0)))
-  else 0
+getTimeV <- function(n, t0) {
+  if (n > 1) {
+    c(0, pop_off(cumsum(t0)))
+  } else {
+    0
+  }
 }
 
-pop_off <- function(.){
-  .[1:{length(.)-1}]
+pop_off <- function(.) {
+  .[1:{
+    length(.) - 1
+  }]
 }
 
 
@@ -240,4 +254,22 @@ calculate_auc <- function(time, concentration) {
   auc <- sum((time[-1] - time[-length(time)]) * (concentration[-1] + concentration[-length(concentration)]) / 2)
 
   return(auc)
+}
+
+
+
+sampleplot <- function() {
+  colss <- sample(grDevices::colors(), 5)
+  plot(c(0, 20, 100),
+    c(0, 20, 100),
+    bg = colss[1:3],
+    xlab = "Sample x",
+    cex = 2,
+    pch = 21,
+    axes = TRUE,
+    ylab = "Sample y",
+    bty = "n"
+  )
+  # box(lwd=2, col=colss[4])
+  text(50, 50, "Click 'Generate data version' to get started", cex = 1.5, pos = 3, col = "red")
 }
