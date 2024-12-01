d467d29cad1f58f97c7b5bdcf3a617cb1fe0ea12
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-12-01 04:30:04 -0500

    Update server.R

--- includes/body/conc.vs.time/server.R
+++ includes/body/conc.vs.time/server.R
@@ -16,6 +16,15 @@
 ##  "individual - spaghetti plot" = 7,
 ##  "individual - scatter plot" = 8
 ##
+##
+##  Statistic (input$graphsummtype)
+##  "Mean" = 1,
+##  "Mean ± SD" = 2,
+##  "Mean ± SEM" = 3,
+##  "Median" = 4,
+##  "Median ± 90% PI" = 5,
+##  "Median ± 95% PI" = 6
+##
 #############################################################################
 #############################################################################
 
@@ -24,22 +33,42 @@ updateSelectInput(session, "datatoUseconc2", choices = data.versions.names)
 
 
 
+
+
+
+
+# Time after first dose plot
 output$concvtimeplot1 <- renderPlot({
   plot.data <- GLOBAL$data.versions[[input$datatoUseconc1]]
   if(!length(plot.data) | is.null(plot.data))return(sampleplot())
   if (nrow(plot.data)) {
-    if (all(c(input$depvar1, input$indepvar, input$depvar3, input$colvar3) %in% c("--",names(plot.data)))) {
+    if (all(c(input$depvar1, input$indepvar, input$cfacetvar, input$colvar3) %in% c("--",names(plot.data)))) {
       updateGraphStatus2()
       plot.data$.dv <- as.numeric(plot.data[[input$depvar1]])
       plot.data$.tm <- as.numeric(plot.data[[input$indepvar]])
       plot.data$.id <- as.numeric(plot.data[[input$idvar]])
       plot.data$.colv <- as.factor(plot.data[[input$colvar3]]) %ifnon% as.factor(plot.data$.id)
-      plot.data$.ttr <- as.factor(plot.data[[input$depvar3]])
+      plot.data$.ttr <- as.factor(plot.data[[input$cfacetvar]])
       plot.data$.none <- "x"
 
+
+      # get data type based on selection
+      datatoplot0 <- plot.data %>% filter(not.na(.dv) & .dv > 0)
+      datatoplot <- datatoplot0
+      if (input$cgraphtype == 3)
+        datatoplot <- data_summarised_overall(datatoplot)
+      if (input$cgraphtype == 6)
+        datatoplot <- data_summarised_facet(datatoplot)
+
+      if(input$cgraphtype %in% c(3,6) & input$graphsummtype %in% 1:3)
+        datatoplot <- datatoplot %>% rename(.dv = dv_mean)
+
+      if(input$cgraphtype %in% c(3,6) & input$graphsummtype %in% 4:6)
+        datatoplot <- datatoplot %>% rename(.dv = dv_med)
+
+
       # global plot out
-      data = plot.data %>% filter(not.na(.dv) & .dv > 0)
-      gplotout <- ggplot(data, aes(.tm, .dv, color = .colv))   +
+      gplotout <- ggplot(datatoplot, aes(.tm, .dv, color = .colv))   +
         guides(color = guide_legend(ncol = input$ncollegend))+
         labs(x = input$labelx, y = input$labely, color = "")+
         theme_bw() +
@@ -47,10 +76,18 @@ output$concvtimeplot1 <- renderPlot({
         styler03 +
         theme(text = element_text(family = input$graphfont), axis.text = element_text(size = input$fontxyticks, family = input$graphfont), axis.title = element_text(size = input$fontxytitle, family = input$graphfont), strip.text = element_text(size = input$fontxystrip, family = input$graphfont), legend.position = input$legendposition, legend.text = element_text(family = input$graphfont), legend.title = element_text(family = input$graphfont), title = element_text(family = input$graphfont))
 
+      # add scatter if plotting median or mean alone
+      if(input$cgraphtype %in% c(3,6) & input$graphsummtype %in% c(1,4))
+        gplotout <- gplotout + geom_point(data = datatoplot0) #+
+        #scale_color_manual(values = rep("black",length(unique(datatoplot0$.id)))) +
+        #theme(legend.position = "none")
+
+
+      # add ribbon if plotting med +/- confident interval
 
       # if color variable is specified
       if (input$colvar3 == "--" | input$cgraphtype %in% c(1,4,7,8)) {
-        gplotout <- gplotout + scale_color_manual(values = rep("black",length(unique(data$.id)))) +
+        gplotout <- gplotout + scale_color_manual(values = rep("black",length(unique(datatoplot$.id)))) +
           theme(legend.position = "none")
       }
 
@@ -94,23 +131,38 @@ output$concvtimeplot1 <- renderPlot({
   }
 })
 
-
+# Time after last dose plot
 output$concvtimeplot2 <- renderPlot({
   plot.data <- GLOBAL$data.versions[[input$datatoUseconc2]]
   if(!length(plot.data) | is.null(plot.data))return(sampleplot())
   if (nrow(plot.data)) {
-    if (all(c(input$depvar1, input$indepvar2, input$depvar3, input$colvar3) %in% c("--",names(plot.data)))) {
+    if (all(c(input$depvar1, input$indepvar2, input$cfacetvar, input$colvar3) %in% c("--",names(plot.data)))) {
       updateGraphStatus2()
       plot.data$.dv <- as.numeric(plot.data[[input$depvar1]])
       plot.data$.tm <- as.numeric(plot.data[[input$indepvar2]])
       plot.data$.id <- as.numeric(plot.data[[input$idvar]])
       plot.data$.colv <- as.factor(plot.data[[input$colvar3]]) %ifnon% as.factor(plot.data$.id)
-      plot.data$.ttr <- as.factor(plot.data[[input$depvar3]])
+      plot.data$.ttr <- as.factor(plot.data[[input$cfacetvar]])
       plot.data$.none <- "x"
 
+
+      # get data type based on selection
+      datatoplot0 <- plot.data %>% filter(not.na(.dv) & .dv > 0)
+      datatoplot <- datatoplot0
+      if (input$cgraphtype == 3)
+        datatoplot <- data_summarised_overall(datatoplot)
+      if (input$cgraphtype == 6)
+        datatoplot <- data_summarised_facet(datatoplot)
+
+      if(input$cgraphtype %in% c(3,6) & input$graphsummtype %in% 1:3)
+        datatoplot <- datatoplot %>% rename(.dv = dv_mean)
+
+      if(input$cgraphtype %in% c(3,6) & input$graphsummtype %in% 4:6)
+        datatoplot <- datatoplot %>% rename(.dv = dv_med)
+
+
       # global plot out
-      data = plot.data %>% filter(not.na(.dv) & .dv > 0)
-      gplotout <- ggplot(data, aes(.tm, .dv, color = .colv))   +
+      gplotout <- ggplot(datatoplot, aes(.tm, .dv, color = .colv))   +
         guides(color = guide_legend(ncol = input$ncollegend))+
         labs(x = input$labelx, y = input$labely, color = "")+
         theme_bw() +
@@ -118,10 +170,18 @@ output$concvtimeplot2 <- renderPlot({
         styler03 +
         theme(text = element_text(family = input$graphfont), axis.text = element_text(size = input$fontxyticks, family = input$graphfont), axis.title = element_text(size = input$fontxytitle, family = input$graphfont), strip.text = element_text(size = input$fontxystrip, family = input$graphfont), legend.position = input$legendposition, legend.text = element_text(family = input$graphfont), legend.title = element_text(family = input$graphfont), title = element_text(family = input$graphfont))
 
+      # add scatter if plotting median or mean alone
+      if(input$cgraphtype %in% c(3,6) & input$graphsummtype %in% c(1,4))
+        gplotout <- gplotout + geom_point(data = datatoplot0) #+
+      #scale_color_manual(values = rep("black",length(unique(datatoplot0$.id)))) +
+      #theme(legend.position = "none")
+
+
+      # add ribbon if plotting med +/- confident interval
 
       # if color variable is specified
       if (input$colvar3 == "--" | input$cgraphtype %in% c(1,4,7,8)) {
-        gplotout <- gplotout + scale_color_manual(values = rep("black",length(unique(data$.id)))) +
+        gplotout <- gplotout + scale_color_manual(values = rep("black",length(unique(datatoplot$.id)))) +
           theme(legend.position = "none")
       }
 
