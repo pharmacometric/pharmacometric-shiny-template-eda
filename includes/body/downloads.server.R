f715ba70872ae3ca226d3e8fc93b4135e574169e
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-12-11 12:24:15 -0500

    Update downloads.server.R

--- includes/body/downloads.server.R
+++ includes/body/downloads.server.R
@@ -10,9 +10,9 @@
 #############################################################################
 
 # download c vs t plot
-output$concvtimedownloadimg = downloadHandler(
+output$concvtimedownloadimg <- downloadHandler(
   filename = function() {
-    fAddDate('app1-eda-concvs-img.png')
+    fAddDate("app1-eda-concvs-img.png")
   },
   content = function(con) {
     ggsave(
@@ -21,42 +21,80 @@ output$concvtimedownloadimg = downloadHandler(
       width = input$downimgw,
       height = input$downimgh,
       scale = input$downimgs,
-      units = 'px'
+      units = "px"
     )
   }
 )
 
 # download c vs t data
-output$concvtimedownloadimg2 = downloadHandler(
+output$concvtimedownloadimg2 <- downloadHandler(
   filename = function() {
-    fAddDate('app1-eda-concvs-obj.data')
+    fAddDate("app1-eda-concvs-obj.data")
   },
   content = function(con) {
-    figx = last_plot()
+    figx <- last_plot()
     save(figx, file = con)
   }
 )
 
 
 # download c vs t code
-output$cdownloadconcvt2 = downloadHandler(
+output$cdownloadconcvt2 <- downloadHandler(
   filename = function() {
-    fAddDate('app1-eda-concvs-tsfd-code.R')
+    fAddDate("app1-eda-concvs-tsfd-code.R.zip")
   },
   content = function(con) {
-    codetempl = readLines(print(GLOBAL$code.convtsfd.tpl))
-    codetempl = gsub("\\{SCRIPTDATA\\}",input[["datatoUseconc1"]],codetempl)
-    codetempl = gsub("\\{LIBRARIES\\}",GLOBAL$code.convtsfd.libs.glue,codetempl)
-    writeLines(codetempl,con)
+    # get template
+    codetempl <- readLines(print(GLOBAL$code.convtsfd.tpl))
+
+    # graph type
+    GRAPHTYPE1 = "dv-tsfd"
+    GRAPHTYPE2 = "all-line"
+
+    # walk through replacements
+    apply(subset(code_download_checks_df), 1, function(g) {
+      #print(g)
+      switch(g["rpl"],
+        "0" = {
+          codetempl <<- gsub(g["srh"], g["with"], codetempl)
+        },
+        "1" = {
+          codetempl <<- gsub(g["srh"], input[[g["with"]]], codetempl)
+        },
+        "2" = {
+          eval(parse(text = paste0(".clogic=input$",g['with']," %in% ",g['with2'])))
+          if(.clogic) codetempl <<- gsub(g['srh'],"",codetempl)
+          else codetempl <<- codetempl[!grepl(g['srh'], codetempl)]
+        },
+        "4" = {
+          if (not.empty(g["with2"])) {
+            codetempl <<- gsub(g["srh"], GLOBAL[[g["with"]]][[input[[g["with2"]]]]], codetempl)
+          } else {
+            codetempl <<- gsub(g["srh"], GLOBAL[[g["with"]]], codetempl)
+          }
+        },
+        "3" = {
+          codetempl <<- gsub(g['srh'],get(g['with']),codetempl)
+        }
+      )
+    })
+
+
+    dirn = tempdir()
+    #unlink(dirn, recursive = TRUE)
+    write.csv(GLOBAL$data.versions[[input$datatoUseconc1]],file.path(dirn,GLOBAL$data.orig.filename))
+    writeLines(codetempl,file.path(dirn,"code.R"))
+    files2zip <- list.files(dirn, full.names = TRUE,pattern = ".R|.csv")
+    zip(zipfile = con, files = files2zip)
   }
 )
 
 
 
 
-output$downloadtable1 = downloadHandler(
+output$downloadtable1 <- downloadHandler(
   filename = function() {
-    fAddDate('app1-res-summ-all.csv')
+    fAddDate("app1-res-summ-all.csv")
   },
   content = function(con) {
     write.csv(summary02(), con)
@@ -65,9 +103,9 @@ output$downloadtable1 = downloadHandler(
 
 
 # download individual summaries
-output$downloadtable1 = downloadHandler(
+output$downloadtable1 <- downloadHandler(
   filename = function() {
-    paste0('app1-res-summ-indv-', Sys.Date(), '.csv')
+    paste0("app1-res-summ-indv-", Sys.Date(), ".csv")
   },
   content = function(con) {
     write.csv(summary01(), con)
@@ -76,9 +114,9 @@ output$downloadtable1 = downloadHandler(
 
 
 # download regimen used for sims
-output$downloadtable3 = downloadHandler(
+output$downloadtable3 <- downloadHandler(
   filename = function() {
-    fAddDate('app1-res-regimen.csv')
+    fAddDate("app1-res-regimen.csv")
   },
   content = function(con) {
     write.csv(data01(), con)
@@ -87,9 +125,9 @@ output$downloadtable3 = downloadHandler(
 
 
 # download individual summaries
-output$downloadtable4 = downloadHandler(
+output$downloadtable4 <- downloadHandler(
   filename = function() {
-    fAddDate('app1-raw_tab_res.csv')
+    fAddDate("app1-raw_tab_res.csv")
   },
   content = function(con) {
     write.csv(GLOBAL$lastsim, con)
