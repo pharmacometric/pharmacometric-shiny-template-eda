0b9e6ecd59b73694cb61ceae711f9ef9d1792b78
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-12-13 06:42:38 -0500

    Update ui.R

--- includes/body/demo.table/ui.R
+++ includes/body/demo.table/ui.R
@@ -12,36 +12,45 @@
 
 body.panel.right.table.demo = card.pro(
 
-  title = "Tables",
+  title = "Descriptive Summary Tables",
   header.bg = "blueLight",
   icon = icon("table"),
   editbtn = 1L,
   collapsed = 1L,
-  sliderInput("selectedrangesumm",
-              "Select treatment time (days) range for summary",
-              value = c(0, 30 * 7), min = 0, max = 30 * 7,
-              width = "90%"
+  div(
+    id = "repttablstatus1",
+    tags$blockquote(style = "color:blue", "Tabs: Demographic table descriptive summary table and others stratified by Stratify variable in Variable Matching tab")
   ),
   tabs = list(
     tabEntry(
       "Demographic table",
+      column(width=4,class ="p-0",selectInput("datatoUseconct1", "Data version to use:", choices = c())),
+      column(width=8,class ="p-0",selectInput("covariatestouse","Variables to use", choices = c(), multiple = 1L, width = "100%")),
       r2resize::splitCard(
-        tableOutput("summaryexptbl"),
+        tableOutput("demdescrtbl"),
         tags$code('Code for the table ...'),
-        position = "vertical"
+        position = "vertical",
+        left.width = "90%"
       )
 
     ),
     tabEntry(
-      "Sampling table",
-      DTOutput("rawrestbl")
-    ),
-    tabEntry(
       "Other tables",
-      DTOutput("summaryrestbl")
+      DTOutput("othrsdescrtbl")
     )
   ),
   sidebar = div(
+      tags$label("Table settings"),
+      selectInput("tabdemgraphtype", "Graph type", choices = c(
+        "overall " = 1,
+        "stratify by treatment" = 2
+      ), selected = "Facet by Group", width = "90%"),
+      conditionalPanel(
+        condition = "input.tabdemgraphtype == 3 | input.tabdemgraphtype == 6",
+        selectInput("graphsummtype", "Statistic", choices = c(
+          "Mean" = 1, "Mean ± SD" = 2, "Mean ± SEM" = 3, "Median" = 4, "Median ± 90% PI" = 5, "Median ± 95% PI" = 6
+        ), selected = "Median ± 90% PI", width = "90%")
+      ),
     tags$label("Table outputs"),
     hr(),
     downloadButton("downloadtable1", "Download summaries", icon = icon("download"), width = "90%"),
@@ -52,8 +61,18 @@ body.panel.right.table.demo = card.pro(
     br(), br(),
     "Download raw output from simulation",
     downloadButton("downloadtable4", "Download raw result", icon = icon("download"), width = "90%")
+  ),
+  footer = list(
+    downloadButton("dtabdownloadimg", "Download table file (docx)", icon = icon("image")),
+    downloadButton("dtabtimedownloadimg2", "Download table object (table1)", icon = icon("image"), class="downloadbtns2"),
+    downloadButton("dtabndloadconcvt2", "Download table code (R)", icon = icon("code"), class="downloadbtns")
   )
 )
+
+
+
+
+
 
 
 
