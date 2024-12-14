4adda03b07064959f59adef20198b514fda71607
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-12-14 05:07:13 -0500

    Update ui.R

--- includes/body/cov.relationship/ui.R
+++ includes/body/cov.relationship/ui.R
@@ -11,57 +11,62 @@
 
 # plot panels
 body.panel.right.plot.cov = card.pro(
-  title = "Covariate Relationship",
-  icon = icon("chart-simple"),
+
+  title = "Covariate Correlation Plots & Tables",
   header.bg = "magenta",
+  icon = icon("chart-simple"),
+  editbtn = 1L,
   collapsed = 1L,
-  xtra.header.content = textOutput("reportgraphstatus2"),
-  selectInput("miscdataUseV1","Data version to use:", choices = data.versions.names),
-  plotOutput("didistPlot", height = 600),
+  div(
+    id = "reptcorrcstatus1",
+    tags$blockquote(style = "color:blue", "Tabs: Continuous refers to the correlation of continuous variables and Categorical refers to the categorical covariates comparison")
+  ),
+  tabs = list(
+    tabEntry(
+      "Continuous",
+      column(width=4,class ="p-0",selectInput("datatoUseconcv1", "Data version to use:", choices = c())),
+      column(width=8,class ="p-0",selectInput("corrcectouse","Variables to use", choices = c(), multiple = 1L, width = "100%")),
+      plotOutput("corrcovplots1", height = 600)
+
+    ),
+    tabEntry(
+      "Categorical",
+      r2resize::splitCard(
+        tableOutput("corrccrfgtbl"),
+        tags$code('Code for the table ...'),
+        position = "vertical",
+        left.width = "90%"
+      )
+
+    )
+  ),
   sidebar = div(
-    tags$label("Graph settings"),
-    selectInput("digraphtype", "Graph type", choices = c(
-      "Combined", "Combined_group", "Facet by ID", "Facet by Group", "Facet by Dose"
+    tags$label("Table settings"),
+    selectInput("corrcdemgraphtype", "Graph type", choices = c(
+      "overall " = 1,
+      "stratify by treatment" = 2
     ), selected = "Facet by Group", width = "90%"),
     conditionalPanel(
-      condition = "input.digraphtype == 'Combined' | input.digraphtype == 'Combined_group' | input.digraphtype == 'Facet by Group'",
-      selectInput("graphtype2", "Statistic", choices = c(
-        "Mean", "Mean ± SD", "Mean ± SEM", "Median", "Median ± 90% PI", "Median ± 95% PI"
+      condition = "input.tabdemgraphtype == 3 | input.tabdemgraphtype == 6",
+      selectInput("graphsummtype", "Statistic", choices = c(
+        "Mean" = 1, "Mean ± SD" = 2, "Mean ± SEM" = 3, "Median" = 4, "Median ± 90% PI" = 5, "Median ± 95% PI" = 6
       ), selected = "Median ± 90% PI", width = "90%")
     ),
-    selectInput("diloglinear", "semi-log or linear", choices = c(
-      "Linear", "semi-log"
-    ), width = "90%"),
-    textInput("dilabely", "Y-label", "Predicted Concentration (μg/ml)", width = "95%"),
-    textInput("dicovlabelx", "X-label", "Time after first dose (days)", width = "95%"),
-    selectInput("digraphfont", "Font type", choices = c(
-      "Times", "Verdana", "Arial", "Courier", "Comic Sans MS"
-    ), selected = "Arial", width = "90%"),
-    sliderInput("difontxytitle",
-                "Font-size title",
-                min = 1,
-                max = 50,
-                value = 14
-    ),
-    sliderInput("difontxyticks",
-                "Font-size ticks",
-                min = 1,
-                max = 50,
-                value = 12
-    ),
-    sliderInput("difontxystrip",
-                "Font-size strip",
-                min = 1,
-                max = 50,
-                value = 12
-    ),
-    "For downloads:",
-    numericInput("didownimgdpi", "Image dpi", 300, width = "90%"),
-    numericInput("didownimgw", "Image width (px)", 2200, width = "90%"),
-    numericInput("didownimgh", "Image height (px)", 1200, width = "90%"),
-    numericInput("didownimgs", "Image scale", 1, width = "90%"),
-    br(),
-    downloadButton("didownloadimg2", "Download plot", icon = icon("image"))
+    tags$label("Table outputs"),
+    hr(),
+    downloadButton("corrcwnloadtable1", "Download summaries", icon = icon("download"), width = "90%"),
+    br(), br(),
+    downloadButton("corrcwnloadtable2", "Download individuals", icon = icon("download"), width = "90%"),
+    br(), br(),
+    downloadButton("corrcwnloadtable3", "Download regimen", icon = icon("download"), width = "90%"),
+    br(), br(),
+    "Download raw output from simulation",
+    downloadButton("corrcwnloadtable4", "Download raw result", icon = icon("download"), width = "90%")
+  ),
+  footer = list(
+    downloadButton("vtabdownloadimg", "Download plot file (ggplot)", icon = icon("image")),
+    downloadButton("vtabtimedownloadimg2", "Download plot object (table1)", icon = icon("image"), class="downloadbtns2"),
+    downloadButton("vtabndloadconcvt2", "Download plot code (R)", icon = icon("code"), class="downloadbtns")
   )
 )
 
