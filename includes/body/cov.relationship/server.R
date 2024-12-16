8b997f2e972de5af0d02c1f8dc91033e1322e920
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-12-15 23:31:27 -0500

    Update server.R

--- includes/body/cov.relationship/server.R
+++ includes/body/cov.relationship/server.R
@@ -2,7 +2,7 @@
 ############################################################################
 ##  Document Path: includes/body/cov.relationship/server.R
 ##
-##  Description: Server function for demographic table
+##  Description: Server function for covariate relationship
 ##
 ##  R Version: 4.4.1 (2024-06-14 ucrt)
 ############################################################################
@@ -16,6 +16,16 @@ updateSelectInput(session, "datatoUseconcv2", choices = data.versions.names)
 
 
 output$corrcovplots1 = renderPlot({
-  my_data <- mtcars[, c(1,3,4,5,6,7)]
+  covrel.data = GLOBAL$data.versions[[input$datatoUseconcv1]]
+  if(covrel.data == NULL){
+    my_data <- mtcars[, c(1,3,4,5,6,7)]
   chart.Correlation(my_data, histogram=TRUE, pch=19)
+  }
+
+  if(nrow(covrel.data) & all(input$corrcectouse %in% names(covrel.data))){
+    varsr = input$corrcectouse
+    covrel.data2 = covrel.data %>% filter(!duplicated(ID)) %>% select(all_of(varsr))
+    chart.Correlation(covrel.data2, histogram=TRUE, pch=19)
+  }
+
 })
