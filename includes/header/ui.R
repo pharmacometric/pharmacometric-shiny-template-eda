936a5318c81735684d43e5a5e74871ca3ec0946f
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-12-22 05:33:56 -0500

    Update ui.R

--- includes/header/ui.R
+++ includes/header/ui.R
@@ -1,6 +1,6 @@
 header.main = titlePanel(tags$div(
   tags$img(src="logo.jpg")," ",
-  "Exploratory Data Analysis for Drug AFR",
+  "Exploratory Data Analysis shiny Template for Drug AFR",
   tags$div(class = "hidden-mobile hidden-tablet pull-right",
              actionButton("aboutproject", "", icon = icon("question")))
 ), windowTitle = "Exploratory Data Analysis")
